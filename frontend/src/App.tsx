a0113757a5df5c34899e3cf7c8b2166134d38032
Hamed <ostovari.eng@gmail.com>
2025-12-01 22:00:58 +0100

    Installed required packages and defined auth and dashboard routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/auth/LoginPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
+import SignupPage from "./pages/auth/SignupPage";
+import NotFoundPage from "./pages/NotFoundPage";
 const App = () => {
-  return <div className="text-7xl text-emerald-600">App</div>;
+  const isAuthorized = false;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthorized ? (
+              <Navigate to="/dashboard" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
