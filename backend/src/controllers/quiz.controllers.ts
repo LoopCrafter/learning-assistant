cbd25dcefa1c1584ddde44595e8929035decc016
Hamed <ostovari.eng@gmail.com>
2025-12-18 20:55:01 +0100

    WIP: implemented quiz endpoint (getQuizzes, getQuizById,submitQuiz)

--- backend/src/controllers/quiz.controllers.ts
+++ backend/src/controllers/quiz.controllers.ts
@@ -1,14 +1,26 @@
 import type { NextFunction, Request, Response } from "express";
+import Quiz from "../models/quiezz.model.js";
 
 // @desc Get quizzes by document ID
 // @route GET /api/quizzes/:documentId
 // @access Private
-const getQuizzes = async (
-  req: Request,
-  response: Response,
-  next: NextFunction
-) => {
+const getQuizzes = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { documentId } = req.params;
+    if (!documentId) {
+      return res.status(400).json({ message: "Document ID is required" });
+    }
+    const quizzes = await Quiz.find({
+      documentId,
+      userId: req.user!._id,
+    }).populate("documentId", "title fieldName");
+
+    res.json({
+      success: true,
+      data: quizzes,
+      message: "Quizzes fetched successfully",
+      count: quizzes.length,
+    });
   } catch (error) {
     next(error);
   }
@@ -17,12 +29,29 @@ const getQuizzes = async (
 // @desc Get quiz by ID
 // @route POST /api/quizzes/quiz/:id
 // @access Private
-const getQuizById = async (
-  req: Request,
-  response: Response,
-  next: NextFunction
-) => {
+const getQuizById = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "Quiz ID is required" });
+    }
+    const quiz = await Quiz.findOne({
+      _id: id,
+      userId: req.user!._id,
+    });
+
+    if (!quiz) {
+      return res.status(404).json({
+        success: false,
+        message: "Quiz not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: quiz,
+      message: "Quiz fetched successfully",
+    });
   } catch (error) {
     next(error);
   }
@@ -31,12 +60,69 @@ const getQuizById = async (
 // @desc Submit quiz answers
 // @route POST /api/quizzes/:id/submit
 // @access Private
-const submitQuiz = async (
-  req: Request,
-  response: Response,
-  next: NextFunction
-) => {
+const submitQuiz = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const { answers } = req.body;
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ message: "Quiz ID is required" });
+    }
+    const quiz = await Quiz.findOne({
+      _id: id,
+      userId: req.user!._id,
+    });
+
+    if (!quiz) {
+      return res.status(404).json({
+        success: false,
+        message: "Quiz not found",
+      });
+    }
+
+    if (quiz.completedAt) {
+      return res.status(400).json({
+        success: false,
+        message: "Quiz already completed",
+      });
+    }
+
+    let correctCount = 0;
+    let userAnswers: typeof quiz.userAnswers = [];
+    answers.forEach(
+      (ans: { questionIndex: number; selectedAnswer: string }) => {
+        const question = quiz.questions[ans.questionIndex];
+        if (question) {
+          const isCorrect = question.correctAnswer === ans.selectedAnswer;
+          if (isCorrect) correctCount++;
+          userAnswers.push({
+            questionIndex: ans.questionIndex,
+            selectedAnswer: ans.selectedAnswer,
+            isCorrect,
+            answeredAt: new Date(),
+          });
+        }
+      }
+    );
+
+    const score = Math.round((correctCount / quiz.totalQuestions) * 100);
+    quiz.userAnswers = userAnswers;
+    quiz.score = score;
+    quiz.completedAt = new Date();
+    await quiz.save();
+
+    res.status(200).json({
+      success: true,
+      data: {
+        quiz: quiz._id,
+        score,
+        totalQuestions: quiz.totalQuestions,
+        correctAnswers: correctCount,
+        percentage: score,
+        userAnswers,
+      },
+      message: "Quiz submitted successfully",
+    });
   } catch (error) {
     next(error);
   }
@@ -47,7 +133,7 @@ const submitQuiz = async (
 // @access Private
 const getQuizResults = async (
   req: Request,
-  response: Response,
+  res: Response,
   next: NextFunction
 ) => {
   try {
@@ -59,11 +145,7 @@ const getQuizResults = async (
 // @desc Delete quiz by ID
 // @route DELETE /api/quizzes/:id
 // @access Private
-const deleteQuiz = async (
-  req: Request,
-  response: Response,
-  next: NextFunction
-) => {
+const deleteQuiz = async (req: Request, res: Response, next: NextFunction) => {
   try {
   } catch (error) {
     next(error);
--- backend/src/routes/quiz.routes.ts
+++ backend/src/routes/quiz.routes.ts
@@ -7,13 +7,30 @@ import {
   getQuizzes,
   submitQuiz,
 } from "../controllers/quiz.controllers.js";
+import { validate } from "../middleware/validate.middleware.js";
+import { body } from "express-validator";
 
 const router = Router();
 router.use(protectRoute);
 
 router.get("/:documentId", getQuizzes);
 router.post("/quiz/:id", getQuizById);
-router.post("/:id/submit", submitQuiz);
+router.post(
+  "/:id/submit",
+  [
+    body("answers")
+      .isArray()
+      .withMessage("Answers must be an array")
+      .custom((value) => {
+        if (value.length === 0) {
+          throw new Error("Answers array cannot be empty");
+        }
+        return true;
+      }),
+  ],
+  validate,
+  submitQuiz
+);
 router.get("/:id/results", getQuizResults);
 router.delete("/:id", deleteQuiz);
 
